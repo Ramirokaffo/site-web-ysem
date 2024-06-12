6afe1890a4deaf2b3846bbcd092e041c7621edc2
ramirokaffo <ramirokaffo>
2024-06-12 18:56:29 +0100

    Redesign page d'accueil

--- student/models.py
+++ student/models.py
@@ -8,8 +8,8 @@ class Student(models.Model):
         ("F", "Femme", )
     )
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, verbose_name="utilisateur correspondant")
-    gender = models.CharField(blank=False, max_length=2, null=False, verbose_name="Sexe", choices=GENDER_CHOICE)
-    born_date = models.DateField(blank=False, null=False, verbose_name='date de naissance')
+    gender = models.CharField(blank=False, max_length=2, null=False, verbose_name="Sexe", default="H", choices=GENDER_CHOICE)
+    born_date = models.DateField(blank=False, null=True, verbose_name='date de naissance')
     phone = models.CharField(blank=True, max_length=15, null=False, verbose_name="numéro de téléphone")
     matricule = models.CharField(blank=True, max_length=15, null=False, verbose_name="matricule de l'étudiant")
     
